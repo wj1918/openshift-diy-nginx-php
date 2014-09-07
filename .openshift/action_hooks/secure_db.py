90279f76f0a548585075bdd1a38b821112170716
Jun Wang <wj1918@hotmail.com>
2014-09-07 01:35:45 -0400

    added install django

---
+++ .openshift/action_hooks/secure_db.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+import os
+import sys
+import django
+from django.contrib.auth.models import User
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'mysite.settings'
+sys.path.append(os.path.join(os.environ['OPENSHIFT_REPO_DIR'], 'wsgi', 'mysite'))
+
+django.setup()
+
+u = User.objects.get(username__exact='admin')
+new_pass="password"
+u.set_password(new_pass)
+u.save()
+# Print the new password info
+#print "Django application credentials:\n\tuser: admin\n\t" + new_pass

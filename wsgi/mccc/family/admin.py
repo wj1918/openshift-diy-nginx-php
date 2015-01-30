38c7583d25ad646dd0037f50586e43deada8e215
wj1918 <wj1918@hotmail.com>
2015-01-30 03:19:12 +0000

    added family link and help text

--- wsgi/mccc/family/admin.py
+++ wsgi/mccc/family/admin.py
@@ -4,51 +4,72 @@ from django.contrib import admin
 
 from family.models import Person
 from family.models import Family
-from family.models import McccDir
 from django.utils.translation import ugettext_lazy as _
 from django.contrib.admin import SimpleListFilter
 
 from django.contrib.admin import SimpleListFilter
 from django.utils.encoding import force_text
 from django.utils.translation import ugettext as _
+from django.core.urlresolvers import reverse
+from django.utils.safestring import mark_safe
 
-def xstr(s):
-    if s is None:
-        return ''
-    return str(s)
-
+def add_link_field(target_model = None, field = '', app='', field_name='link',
+                   link_text=unicode):
+    def add_link(cls):
+        reverse_name = target_model or cls.model.__name__.lower()
+        def link(self, instance):
+            app_name = app or instance._meta.app_label
+            reverse_path = "admin:%s_%s_change" % (app_name, reverse_name)
+            link_obj = getattr(instance, field, None) or instance
+            url = reverse(reverse_path, args = (link_obj.id,))
+            return mark_safe("<a href='%s'>%s</a>" % (url, link_text(link_obj)))
+        link.allow_tags = True
+        link.short_description = reverse_name 
+        setattr(cls, field_name, link)
+        cls.readonly_fields = list(getattr(cls, 'readonly_fields', [])) + \
+            [field_name]
+        return cls
+    return add_link
+    
 class PersonInline(admin.StackedInline):
     model = Person
     extra = 0
 
 class FamilyAdmin(admin.ModelAdmin):
-    list_display = ('familyid','address','city','state','zip','status', 'home1','home2', 'homefax')
+    list_display = ('id','address','city','state','zip','status', 'home1','home2', 'homefax')
     list_filter = ['status','city','state']
-    search_fields = ['familyid','address','city','home1']
+    search_fields = ['id','address','city','home1']
     inlines = [PersonInline]
         
+@add_link_field('family','family',field_name='link2')
 class PersonAdmin(admin.ModelAdmin):
-    list_display = ('personid','last','first','middle','chinese','sex','role','get_family_address','get_family_phone','email','cphone','wphone','worship','fellowship','fellowship2','baptized','bapday','category','birthday','member','memday','get_family_id','get_family_status')
+    list_display = ('id','last','first','middle','chinese','sex','role','link2','get_family_phone','email','cphone','wphone','worship','fellowship','fellowship2','baptized','bapday','category','birthday','member','memday','get_family_id','get_family_status')
     list_filter = ['fellowship','fellowship2','worship','baptized','member', 'family__status', 'family__city',]
     search_fields = ['last','first','chinese','email','comment', "family__address", "family__city", "family__state", "family__zip","family__home1","family__home2","family__homefax",]
-
+    raw_id_fields = ("family",)
+    
     def get_family_status(self, obj):
         return obj.family.status
     get_family_status.short_description = 'Family Status'
     get_family_status.admin_order_field = 'family__status'
 
     def get_family_id(self, obj):
-        return obj.family.familyid
+        return obj.family.id
     get_family_id.short_description = 'Family Id'
-    get_family_id.admin_order_field = 'family__familyid'
+    get_family_id.admin_order_field = 'family__id'
 
     def get_family_address(self, obj):
-        return xstr(obj.family.address) +","+xstr(obj.family.city)+ ","+ xstr(obj.family.state) + ","+ xstr(obj.family.zip)
+        list1 = [obj.family.address, obj.family.city, obj.family.state, obj.family.zip]
+        faddress =[x for x in list1 if x is not None]
+        return u','.join(faddress).encode('utf-8').strip()
+        
     get_family_address.short_description = 'Family Address'
     get_family_address.admin_order_field = 'family__address'
 
     def get_family_phone(self, obj):
-        return xstr(obj.family.home1) +" "+xstr(obj.family.home2)+ " "+ xstr(obj.family.homefax)
+        list1 = [obj.family.home1, obj.family.home2, obj.family.homefax]
+        fphones =[x for x in list1 if x is not None]
+        return u' '.join(fphones).encode('utf-8').strip()
     get_family_phone.short_description = 'Family Phone'
     get_family_phone.admin_order_field = 'family__home1s'
 
--- wsgi/mccc/family/models.py
+++ wsgi/mccc/family/models.py
@@ -1,17 +1,15 @@
 from django.db import models
 
 # Create your models here.
-def xstr(s):
-    if s is None:
-        return ''
-    return str(s)
 
 class Family(models.Model):
-    def __str__(self):
-        return xstr(self.address) +","+xstr(self.city)+ ","+ xstr(self.state) + ","+ xstr(self.zip)
-        
-    familyid = models.AutoField(db_column='FamilyID',primary_key=True)
-    status = models.CharField(db_column='Status', max_length=2, blank=True)  # Field name made lowercase.
+    def __unicode__(self):
+        list0 = [self.address, self.city, self.state, self.zip]
+        faddress =[x for x in list0 if x is not None]
+        return u','.join(faddress).encode('utf-8').strip()
+
+    id = models.AutoField(db_column='FamilyID',primary_key=True,verbose_name="Family ID")
+    status = models.CharField(db_column='Status', max_length=2, blank=True, help_text="A -- Active, I -- Inactive, L -- Local Inactive, O -- Out of Date, N -- New, R -- Remote")  # Field name made lowercase.
     home1 = models.CharField(db_column='Home1', max_length=40, blank=True)  # Field name made lowercase.
     home2 = models.CharField(db_column='Home2', max_length=40, blank=True)  # Field name made lowercase.
     homefax = models.CharField(db_column='HomeFax', max_length=40, blank=True)  # Field name made lowercase.
@@ -24,12 +22,11 @@ class Family(models.Model):
         managed = False
         db_table = 'MCCC_Family'
 
-
 class Person(models.Model):
-    def __str__(self):
+    def __unicode__(self):    
         return '{0}, {1}'.format(self.first, self.last)
     
-    personid = models.AutoField(db_column='PersonID',primary_key=True)
+    id = models.AutoField(db_column='PersonID',primary_key=True,verbose_name="Person ID")
     last = models.CharField(db_column='Last', max_length=30, blank=True)  # Field name made lowercase.
     first = models.CharField(db_column='First', max_length=40, blank=True)  # Field name made lowercase.
     middle = models.CharField(db_column='Middle', max_length=20, blank=True)  # Field name made lowercase.
@@ -41,15 +38,15 @@ class Person(models.Model):
     wphone = models.CharField(db_column='WPhone', max_length=40, blank=True)  # Field name made lowercase.
     wfax = models.CharField(db_column='WFax', max_length=40, blank=True)  # Field name made lowercase.
     waddress = models.CharField(db_column='Waddress', max_length=100, blank=True)  # Field name made lowercase.
-    worship = models.CharField(db_column='Worship', max_length=4, blank=True)  # Field name made lowercase.
+    worship = models.CharField(db_column='Worship', max_length=4, blank=True, help_text="C -- Cantonese, E -- English, M -- Mandarin")  # Field name made lowercase.
     fellowship = models.CharField(db_column='Fellowship', max_length=50, blank=True)  # Field name made lowercase.
     fellowship2 = models.CharField(db_column='Fellowship2', max_length=50, blank=True)  # Field name made lowercase.
-    baptized = models.CharField(db_column='Baptized', max_length=4, blank=True)  # Field name made lowercase.
+    baptized = models.CharField(db_column='Baptized', max_length=4, blank=True, help_text="Y -- Yes, N -- No, YM -- MCCC Baptized, U -- Unknown")  # Field name made lowercase.
     bapday = models.CharField(db_column='BapDay', max_length=510, blank=True)  # Field name made lowercase.
     category = models.CharField(db_column='Category', max_length=4, blank=True)  # Field name made lowercase.
     birthday = models.DateField(db_column='Birthday', blank=True, null=True)  # Field name made lowercase.
     anniday = models.DateField(db_column='AnniDay', blank=True, null=True)  # Field name made lowercase.
-    member = models.CharField(db_column='Member', max_length=2, blank=True)  # Field name made lowercase.
+    member = models.CharField(db_column='Member', max_length=2, blank=True, help_text="Y -- Active, N -- No, I -- Inactive, J -- Jr Member, U -- Unknown, D -- Deceased")  # Field name made lowercase.
     memday = models.DateField(db_column='MemDay', blank=True, null=True)  # Field name made lowercase.
     comment = models.TextField(db_column='Comment', blank=True, null=True)  # Field name made lowercase.
     family = models.ForeignKey(Family,db_column='FamilyID',related_name="persons")
@@ -57,19 +54,3 @@ class Person(models.Model):
     class Meta:
         managed = False
         db_table = 'MCCC_Person'
-
-class McccDir(models.Model):
-    family_id = models.IntegerField(db_column='FamilyID',primary_key=True)  # Field name made lowercase.
-    last_nm = models.CharField(db_column='Last_NM', max_length=30, blank=True)  # Field name made lowercase.
-    first_nm = models.CharField(db_column='First_NM', max_length=40, blank=True)  # Field name made lowercase.
-    chinese_nm = models.CharField(db_column='Chinese_NM', max_length=20, blank=True)  # Field name made lowercase.
-    wf_first = models.CharField(db_column='WF_First', max_length=40, blank=True)  # Field name made lowercase.
-    wf_chinese_nm = models.CharField(db_column='WF_Chinese_NM', max_length=20, blank=True)  # Field name made lowercase.
-    home_phone = models.CharField(db_column='Home_Phone', max_length=40, blank=True)  # Field name made lowercase.
-    work_phone = models.CharField(db_column='Work_Phone', max_length=83, blank=True)  # Field name made lowercase.
-    address = models.CharField(db_column='Address', max_length=130, blank=True)  # Field name made lowercase.
-    worship = models.CharField(db_column='Worship', max_length=9, blank=True)  # Field name made lowercase.
-
-    class Meta:
-        managed = False
-        db_table = 'MCCC_Dir'
